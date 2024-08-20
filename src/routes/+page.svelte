29f8f4a0d2ef659d61826980b244dcd4cbb1b23a
huise0ng <shinhuiseong07@gmail.com>
2024-08-20 15:57:14 +0900

    ADD :: 초기세팅 + 시작 페이지 퍼블리싱

---
+++ src/routes/+page.svelte
@@ -0,0 +1,59 @@
+<script>
+    import { goto } from '$app/navigation';
+
+    function handleSignupClick() {
+        goto('section'); 
+    }
+</script>
+
+<style>
+.container {
+    width: 375px;
+    height: 812px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    background-color: #FFF5FD;
+    font-family: 'Pretendard', sans-serif;
+    margin: 0 auto;
+    overflow: hidden;
+    border-radius: 32px;
+    margin: 40px auto 0; 
+}
+
+.logo {
+    width: 190px;
+    height: auto;
+    position: absolute;
+    top: 300px;
+}
+
+.signup-button {
+    width: 310px;
+    height: 50px;
+    background-color: #FF3E7A;
+    border: none;
+    border-radius: 8px;
+    color: #FFFFFF;
+    font-size: 18pt;
+    font-family: 'Pretendard', sans-serif;
+    font-weight: 400;
+    cursor: pointer;
+    position: absolute;
+    bottom: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+
+</style>
+
+<div class="container"> 
+    <img src="/logo.svg" alt="Logo" class="logo">
+    <button class="signup-button" on:click={handleSignupClick}>
+        회원가입
+    </button>
+</div>
---
+++ src/routes/sent/+page.svelte
@@ -0,0 +1,71 @@
+<script>
+
+  </script>
+  
+  <style>
+    .container {
+        width: 375px;
+        height: 812px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        position: relative;
+        background-color: #FFF5FD;
+        font-family: 'Pretendard', sans-serif;
+        margin: 0 auto;
+        overflow: hidden;
+        border-radius: 32px;
+        margin: 40px auto 0; 
+    }
+  
+    h1 {
+      color: #FF3E7A;
+      font-size: 28px;
+      font-weight: 700px;
+      margin-bottom: 1px;
+      margin-right: 50px;
+    }
+  
+    p {
+      color: #732F4B;
+      font-size: 18px;
+      margin-bottom: 50px;
+      margin-right: 100px;
+      margin-top: 3px;
+    }
+  
+    .image {
+      margin-bottom: 2rem;
+    }
+  
+    button {
+        width: 310px;
+        height: 50px;
+        background-color: #FF3E7A;
+        border: none;
+        border-radius: 8px;
+        color: #FFFFFF;
+        font-size: 18pt;
+        font-family: 'Pretendard', sans-serif;
+        font-weight: 400;
+        cursor: pointer;
+        position: absolute;
+        bottom: 100px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+  
+    button:hover {
+      background-color: #e03570;
+    }
+  </style>
+  
+  <div class="container">
+    <h1>미래로 편지를 보냈어.</h1>
+    <p>편지가 도착하면 알려줄게.</p>
+    <img src="/sent.svg" alt="Sent" class="image" />
+    <button on:click={() => window.location.href = '/'}>홈으로</button>
+  </div>
+  
